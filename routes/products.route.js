c8fa0506fc0663f48433fc308b9cc456f487d7f1
Brooklyne Cheney <brookecheney@hotmail.com>
2019-03-01 22:43:32 -0700

    Add files via upload

---
+++ routes/products.route.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+// Require the controllers WHICH WE DID NOT CREATE YET!!
+const product_controller = require('../controllers/products.controller');
+
+
+// a simple test url to check that all of our files are communicating correctly.
+router.get('/test', product_controller.test);
+module.exports = router;
+
+
+// routes/products.route.js
+
+router.post('/create', product_controller.product_create);
+
+router.get('/:id', product_controller.product_details);
+
+
+
+// routes/products.route.js
+
+router.put('/:id/update', product_controller.product_update);
+
+// routes/products.route.js
+
+router.delete('/:id/delete', product_controller.product_delete);
